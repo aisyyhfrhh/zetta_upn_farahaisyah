d1c39caf8ab7d98c83acceca0555f6132c1f4206
aisyyhfrhh <123200009@student.upnyk.ac.id>
2023-03-14 21:39:34 +0700

    javascript day 2

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const buku={
     "nama": "Laut Bercerita",
     "harga": "105000",
     "diskon": "20",
-    "ready":true
+    "ready":true,
+    "stock": "20",
+    "order": "1" 
 }
 
-function Beli({nama, harga, diskon, ready}, tax){
+function Beli({nama, harga, diskon, ready, stock, order }, tax){
     const discount = harga *(diskon/100);
     const setelahDiskon = harga - discount;
     const pajak = setelahDiskon * (tax/100);
     const total = setelahDiskon + pajak;
+    let i = stock;
+    let n = order;
+
+    while (i > 0) {
+        totalPrice = taxTotal + totalPrice;
+        bookBuyed = bookBuyed + 1;
+        i = i - 1;
+        n = n - 1;
+
+        if (i == 0 || n == 0) {
+            break;
+        }
+    }
 
     console.log('Judul          : ', nama);
     console.log('Harga          : ', harga);
@@ -23,8 +38,20 @@ function Beli({nama, harga, diskon, ready}, tax){
     console.log('Pajak          : ', tax);
     console.log('Total          : ', total);
 
+    if (i == 0 && n > 0) {
+        console.log('that book is already out of stock!');
+        console.log('sisa buku tidak terbeli adalah ', n, ' pcs');
+
+    } else {
+        console.log('semua pesanan terbeli :D!');
+        console.log('sisa stock buku adalah ', i, ' pcs');
+    }
+
 
     return 0;
 
 
 }
+
+
+Beli(buku, 10);
